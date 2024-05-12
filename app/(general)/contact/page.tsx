a2d707e73623a624db7004bd446cb7b77541c497
Fernando Ibarra <Fernandooibarra@gmail.com>
2024-05-11 22:09:54 -0700

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+
+
+export const metadata = {
+ title: 'Contact Page',
+ description: 'Contact Description',
+};
+
+export default function ContactPage() {
+    return (
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
